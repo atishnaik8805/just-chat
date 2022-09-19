efbc55bfcebcd853f222f3a6769254d40bc9be4d
atishNaik <atishnaik8805@gmail.com>
2022-09-19 18:32:32 +0530

    logout button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   contacts: any = [{
     'name': 'amith',
@@ -26,6 +27,14 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  logout(e:any) {
+    if (confirm('Do you want to logout?')) {
+      localStorage.removeItem('access_token');
+      this.router.navigateByUrl('/login')
+    }
+    
   }
 
 }
